bf18426f79e6dc51afe5f0092b7ba39b0830f88d
osmansmith <osman_ahumada@hotmail.com>
2017-10-24 11:08:57 -0300

    AVANCE

--- Corriente.java
+++ Corriente.java
@@ -8,5 +8,5 @@
 public class Corriente extends Vista
 {
    public Corriente()
-   {}
+   {}   
 }
--- Simulacion.java
+++ Simulacion.java
@@ -5,12 +5,13 @@
  * @author (Osman Ahumada) 
  * @version (1.0)
  */
-
+import java.util.Scanner;
 public class Simulacion
 {
     
     public static void main(String[] args)
     {
+        Scanner teclado = new Scanner(System.in);
         Vista cvista = new Vista();
         Corriente ccorriente = new Corriente();
         Ahorro cahorro = new Ahorro();
@@ -18,14 +19,35 @@ public class Simulacion
 
         int depo = 50000;
         int giro = 45000;
-        
-        System.out.println("Cuenta Vista");        
-        System.out.println("");        
-        System.out.println("Deposito :  "+depo);
-        cvista.depositar(depo);
-        System.out.println("Giro  :  "+giro);        
-        cvista.girar(giro);
-        System.out.println("Saldo disponible :  "+cvista.consultar());        
+        int op = 5;
+        while(op>0)
+        {
+         System.out.print('\u000C');
+         System.out.println("[1] Cuenta Vista.");
+         System.out.println("[2] Cuenta Corriente.");
+         System.out.println("[3] Cuenta de Ahorro.");
+         System.out.println("[4] Targeta de Credito.");
+         System.out.println("[0] Salir.");    
+         op = teclado.nextInt();
+         switch(op)
+         {
+          case 1:
+          break;
+          case 2:
+          break;
+          case 3:
+          break;
+          case 4:
+          break;
+          case 0:
+          op = 0;
+          break;
+          default:
+          break;
+         }
+        }
+        System.out.print('\u000C');
+        System.out.println("Ádios");      
         
     }
 }
