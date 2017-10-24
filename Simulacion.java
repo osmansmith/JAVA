306d9e61139a24ceeb5d03781ee6245597722f35
Osman AV <osman_ahumada@hotmail.com>
2017-10-24 01:04:15 -0400

    ejemplo con cuenta vista

--- Simulacion.java
+++ Simulacion.java
@@ -5,10 +5,27 @@
  * @author (Osman Ahumada) 
  * @version (1.0)
  */
+
 public class Simulacion
 {
-    Vista cvista = new Vista();
-    Corriente ccorriente = new Corriente();
-    Ahorro cahorro = new Ahorro();
-    TargetaCredito card = new TargetaCredito();
+    
+    public static void main(String[] args)
+    {
+        Vista cvista = new Vista();
+        Corriente ccorriente = new Corriente();
+        Ahorro cahorro = new Ahorro();
+        TargetaCredito card = new TargetaCredito();
+
+        int depo = 50000;
+        int giro = 45000;
+        
+        System.out.println("Cuenta Vista");        
+        System.out.println("");        
+        System.out.println("Deposito :  "+depo);
+        cvista.depositar(depo);
+        System.out.println("Giro  :  "+giro);        
+        cvista.girar(giro);
+        System.out.println("Saldo disponible :  "+cvista.consultar());        
+        
+    }
 }
